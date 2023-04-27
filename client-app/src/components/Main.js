afcf2fc9d61d9b6b1d3f1ba9655138fa2e045362
bahim22 <78245175+bahim22@users.noreply.github.com>
2023-04-27 17:06:30 -0400

    edit frontend dir with create react app, bootstrap

---
+++ client-app/src/components/Main.js
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavbarBrand from 'react-bootstrap/NavbarBrand';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const MainNav = (author, links, ...rest) => {
+	const author = [
+		{
+			id: 1,
+			img: '/himacard.png',
+			name: 'Hima Balde'
+		}
+	]
+	const links = 'https://github.com/bahim22/bahim22'
+
+	return (
+		<Container>
+			
+		</Container>
+	)
+}
+
+export default MainNav;
