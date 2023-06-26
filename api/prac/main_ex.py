5bd64f59e98925e04c58957482bc700df17d0df4
bahim22 <78245175+bahim22@users.noreply.github.com>
2023-06-26 16:09:17 -0400

    db and fastapi r&d + work on connecting and creating diff db

--- api/prac/main_ex.py
+++ api/prac/main_ex.py
@@ -1,10 +1,12 @@
 # for PostgreSQL Server
+
 from contextlib import asynccontextmanager
 from typing import List
 import databases
 import sqlalchemy
 from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 from pydantic import BaseModel
 import os
 from urllib.parse import quote_plus
@@ -58,10 +60,58 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.add_middleware(GZipMiddleware)
 
 
-@app.post('/notes/', response_model=Note, status_code=status.HTTP_201_CREATED)
+""" @app.post('/notes/', response_model=Note, status_code=status.HTTP_201_CREATED)
+async def create_note(note: NoteIn):  # type: ignore
+    query = notes.insert().values(text=note.text, completed=note.completed)
+    last_record_id = await database.execute(query)
+    return {**note.dict(), 'id': last_record_id} """
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+@app.post("/notes/", response_model=Note,
+          status_code=status.HTTP_201_CREATED)
 async def create_note(note: NoteIn):
     query = notes.insert().values(text=note.text, completed=note.completed)
     last_record_id = await database.execute(query)
-    return {**note.dict(), 'id': last_record_id}
+    return {**note.dict(), "id": last_record_id}
+
+
+@app.put("/notes/{note_id}/", response_model=Note,
+         status_code=status.HTTP_200_OK)
+async def update_note(note_id: int, payload: NoteIn):
+    query = notes.update().where(notes.c.id == note_id).values(
+        text=payload.text, completed=payload.completed)
+    await database.execute(query)
+    return {**payload.dict(), "id": note_id}
+
+
+@app.get("/notes/", response_model=List[Note],
+         status_code=status.HTTP_200_OK)
+async def read_notes(skip: int = 0, take: int = 20):
+    query = notes.select().offset(skip).limit(take)
+    return await database.fetch_all(query)
+
+
+@app.get("/notes/{note_id}/", response_model=Note,
+         status_code=status.HTTP_200_OK)
+async def read_notes_id(note_id: int):
+    query = notes.select().where(notes.c.id == note_id)
+    return await database.fetch_one(query)
+
+
+@app.delete("/notes/{note_id}/", status_code=status.HTTP_200_OK)
+async def delete_note(note_id: int):
+    query = notes.delete().where(notes.c.id == note_id)
+    await database.execute(query)
